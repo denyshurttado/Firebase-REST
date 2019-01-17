acccb8247bb088d92633590d6bfa229b8cd8faf9
DenysDenylson <denysdenylson@gmail.com>
2019-01-17 17:58:31 -0400

    Componentes heroes y show-heroe

---
+++ src/app/components/heroes/show-heroe.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-show-heroe',
+  templateUrl: './show-heroe.component.html',
+  styles: []
+})
+export class ShowHeroeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
